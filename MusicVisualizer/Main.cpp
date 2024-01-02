d982e28844d45ab82624f71b17434bed8b582bcc
June Min Htet <72848103+LootGodamn@users.noreply.github.com>
2024-01-03 01:35:34 +0630

    main.cpp made

---
+++ MusicVisualizer/Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <raylib.h>
+#include <raymath.h>
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
+
+using namespace std;
+
+int main() {
+
+	BeginDrawing();
+	EndDrawing();
+	return 0;
+}
